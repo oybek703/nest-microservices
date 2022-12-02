8b4c5b301302426ba801b39952ec350d6d116188
Yoriqulov Oybek <hhoybek@gmail.com>
2022-12-02 13:36:57 +0500

    rmq validation added

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Body, Controller } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AccountLogin, AccountRegister } from '@nest-microservices/contracts'
-import { RMQRoute } from 'nestjs-rmq'
+import { RMQRoute, RMQValidate } from 'nestjs-rmq'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @RMQValidate()
   @RMQRoute(AccountRegister.topic)
   async register(@Body() dto: AccountRegister.Request): Promise<AccountRegister.Response> {
     return this.authService.register(dto)
   }
 
+  @RMQValidate()
   @RMQRoute(AccountLogin.topic)
   async login(@Body() dto: AccountLogin.Request): Promise<AccountLogin.Response> {
     const { id } = await this.authService.validateUser(dto.email, dto.password)
--- libs/contracts/src/lib/account/account.register.ts
+++ libs/contracts/src/lib/account/account.register.ts
@@ -1,9 +1,17 @@
+import { IsEmail, IsOptional } from 'class-validator'
+
 export namespace AccountRegister {
   export const topic = 'account.register.command'
 
   export class Request {
+    @IsEmail()
     email: string
+
+    @IsEmail()
     password: string
+
+    @IsOptional()
+    @IsEmail()
     displayName?: string
   }
 
