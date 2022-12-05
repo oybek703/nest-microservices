c1772d6bd25f8e64d9ca620161d5f62f470be928
Yoriqulov Oybek <hhoybek@gmail.com>
2022-12-05 18:01:29 +0500

    cron job added

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { getRMQConfig } from './configs/rmq.config'
 import { JwtModule } from '@nestjs/jwt'
 import { getJWTConfig } from './configs/jwt.config'
 import { PassportModule } from '@nestjs/passport'
+import { ScheduleModule } from '@nestjs/schedule'
+import { UserController } from './controller/user.controller'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: 'envs/.api.env', isGlobal: true }),
     RMQModule.forRootAsync(getRMQConfig()),
     JwtModule.registerAsync(getJWTConfig()),
-    PassportModule
+    PassportModule,
+    ScheduleModule.forRoot()
   ],
-  controllers: [AuthController]
+  controllers: [AuthController, UserController]
 })
 export class AppModule {}
--- apps/api/src/app/controller/user.controller.ts
+++ apps/api/src/app/controller/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import { Controller, Get, Logger, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from '../guards/jwt.guard'
 import { UserId } from '../guards/user.decorator'
+import { Cron } from '@nestjs/schedule'
 
 @Controller('user')
-export class AuthController {
+export class UserController {
   constructor() {}
 
   @UseGuards(JwtAuthGuard)
   @Get('info')
   async getUser(@UserId() userId) {}
+
+  @Cron('*/5 * * * * *')
+  async cron() {
+    Logger.log('Done')
+  }
 }
